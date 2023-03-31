7e489278028719e1582c11b816cd599a79f9e9e4
Arpit kumar <92947276+2001Arpit@users.noreply.github.com>
2023-04-01 00:40:01 +0530

    decoder and rs1 tb

--- rtl/rs1_register.v
+++ rtl/rs1_register.v
@@ -1,13 +1,13 @@
-module rs1_register(clk,rst, opcode, write_en, read_en, write_data_decoder, write_data_regfile, read_data);
+module rs1_register(clk,rst, opcode, write_data_decoder, write_data_regfile, read_data);
 
 input clk,rst;
-input read_en, write_en;
 input [6:0] opcode;                 //check if is_r, is_i possible
 
-input [31:0] write_data;
 output reg [31:0] read_data;
 
-reg mem[31:0];
+input [31:0] write_data_regfile,write_data_decoder;
+
+reg [31:0] mem;
 
 always @(posedge clk) begin
 
@@ -16,9 +16,9 @@ if(opcode[6:3] === 4'b0000||             //i type check
    opcode[6:2] === 5'b11001) 
 mem <= write_data_decoder;
 
-else if(opcode[6:3] === 4'b0000||             //r type check
-   opcode[6:4] === 3'b001||
-   opcode[6:2] === 5'b11001) 
+else if(opcode[6:4] === 3'b011||
+        opcode[6:2] === 5'b01011||
+        opcode[6:2] === 5'b10100)
 mem <= write_data_regfile;
 
 end
