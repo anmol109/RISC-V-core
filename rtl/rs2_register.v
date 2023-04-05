316479312205d1cf02ba4fcc834a3ed429131f97
Arpit kumar <92947276+2001Arpit@users.noreply.github.com>
2023-04-05 11:42:28 +0530

    rs2 tb not working

--- rtl/rs2_register.v
+++ rtl/rs2_register.v
@@ -2,10 +2,10 @@ module rs2_register(clk,rst,rs2_write_data, rs2_read_data);
 
 input clk,rst;
 
-input [31:0] write_data;
+input [31:0] rs2_write_data;
 output reg [31:0] rs2_read_data;
 
-reg mem[31:0];
+reg [31:0] mem;
 
 always @(posedge clk) begin
   
